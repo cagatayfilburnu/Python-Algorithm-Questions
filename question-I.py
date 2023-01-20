4986f9a07791fa082b400443bf2cbde8a2d95c77
Çağatay Filburnu <95078183+cagatayfilburnu@users.noreply.github.com>
2023-01-20 19:15:51 +0300

    Update question-I.py

--- question-I.py
+++ question-I.py
@@ -1,8 +1,8 @@
 #################################################################
 # Print every number from 1 to 100 in a new line.
-# Print "fizz" instead of numbers for each floor of 3.
-# Print "buzz" instead of the number for each floor of 5.
-# Print "fizzbuzz" instead of numbers for numbers that are multiple of 3 and 5.
+# Print "fizz" instead of numbers that can be divisible to 3.
+# Print "buzz" instead of numbers that can be divisible to 5.
+# Print "fizzbuzz" instead of numbers that are multiple of 3 and 5.
 #################################################################
 
 for i in range(0, 101):
